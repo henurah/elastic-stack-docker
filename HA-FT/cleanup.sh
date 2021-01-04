4d22fc6e438593a37a9233c76c52c129b9404ff2
Henry Pak <henry@elastic.co>
2021-01-03 19:21:55 -0800

    Docker containers to use default ports and export unique port to host

---
+++ HA-FT/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# -v option removes volumes
+docker-compose down -v
+
+#docker volume rm es_certs
+#docker volume rm es_data01
+#docker volume rm es_data02
+#docker volume rm es_data03
